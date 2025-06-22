e4365a17f0f9fef5080121f88863b3b5f94e92dd
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-06-22 21:26:31 +0600

    start

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 8,
-            onSale: false
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester']
         }
     }
 })
