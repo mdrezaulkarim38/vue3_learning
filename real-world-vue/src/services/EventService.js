3ef22629b56c73b3dd438c6be536beb24ee4edc4
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-06-25 22:15:47 +0600

    little change

--- real-world-vue/src/services/EventService.js
+++ real-world-vue/src/services/EventService.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/mdrezaulkarim38/vue3_learning/events',
-    withCredentials: false,
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    }
+  baseURL: 'https://my-json-server.typicode.com/mdrezaulkarim38/vue3_learning',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
 })
 
 export default {
-    getEvents() {
-        return apiClient.get('/events')
-    }
+  getEvents() {
+    return apiClient.get('/events') 
+  }
 }
