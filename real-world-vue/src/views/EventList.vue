26c680a5dd5e97c48ad8e99fb68ecdc188a2900b
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-06-29 20:59:03 +0600

    now working

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,15 +1,20 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch, computed } from 'vue'
 
 const props = defineProps(['page'])
-const events = ref(null)
-
+const events = ref(null);
+const totalEvents = ref(0);
+const hasNextPage = computed(()=>{
+  const totalPages = Math.ceil(totalEvents.value / 2)
+  return props.page < totalPages
+})
 const fetchEvents = () => {
   EventService.getEvents(2, props.page)
     .then((response) => {
-      events.value = response.data
+      events.value = response.data;
+      totalEvents.value = response.headers["x-total-count"]
     })
     .catch((error) => {
       console.log(error)
@@ -18,21 +23,26 @@ const fetchEvents = () => {
 onMounted(() => {
   fetchEvents()
 })
+
+watch(
+  () => props.page,
+  () => {
+    events.value = null
+    fetchEvents()
+  }
+)
 </script>
 
 <template>
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
-    <router-link
-      :to="{ name: 'EventList', query: { page: page - 1 } }"
-      rel="prev"
-      v-if="page != 1"
-    >Pre Page</router-link>
-    <router-link
-      :to="{ name: 'EventList', query: { page: page + 1 } }"
-      rel="next"      
-    >Next Page</router-link>
+    <router-link :to="{ name: 'EventList', query: { page: page - 1 } }" rel="prev" v-if="page != 1"
+      >Pre Page</router-link
+    >
+    <router-link :to="{ name: 'EventList', query: { page: page + 1 } }" rel="next" v-if="hasNextPage"
+      >Next Page</router-link
+    >
   </div>
 </template>
 
