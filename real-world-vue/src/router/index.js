f0d0cc36aad842dd1cdeff91f4c1f711d1eebb0e
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-02 21:21:00 +0600

    make some change in here

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -1,45 +1,53 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "@/views/EventList.vue";
-import EventDetails from "@/views/event/Details.vue";
-import EventEdit from "@/views/event/Edit.vue"
-import EventRegister from "@/views/event/Register.vue"
-import About from "@/views/About.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '@/views/EventList.vue'
+import EventDetails from '@/views/event/Details.vue'
+import EventEdit from '@/views/event/Edit.vue'
+import EventRegister from '@/views/event/Register.vue'
+import About from '@/views/About.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "EventList",
+    path: '/',
+    name: 'EventList',
     component: EventList,
-    props: route => ({ page: parseInt(route.query.page) || 1 })
+    props: (route) => ({ page: parseInt(route.query.page) || 1 }),
   },
   {
-    path: "/event/:id",
-    name: "EventDetails",
+    path: '/event/:id',
+    name: 'EventLayout',
     props: true,
-    component: EventDetails,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails,
+      },
+      {
+        path: '/event/:id/register',
+        name: 'EventRegister',
+      
+        component: EventRegister,
+      },
+      {
+        path: '/event/:id/edit',
+        name: 'EventEdit',
+    
+        component: EventEdit,
+      },
+    ],
   },
+
   {
-    path: "/event/:id/register",
-    name: "EventRegister",
-    props: true,
-    component: EventRegister,
-  },
-  {
-    path: "/event/:id/edit",
-    name: "EventEdit",
-    props: true,
-    component: EventEdit,
-  },
-  {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     component: About,
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+export default router
