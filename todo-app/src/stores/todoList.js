10a27253a1488951c93c279cbc6e5b98e33f10a1
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-14 20:58:20 +0600

    now need to work in todo form

--- todo-app/src/stores/todoList.js
+++ todo-app/src/stores/todoList.js
@@ -3,8 +3,18 @@ import { defineStore } from 'pinia'
 export const useTodoListStore = defineStore('todoList', {
     // state
     state: () => ({
-        TodoList: []
-    })
+        todoList: []
+    }),
     // getters
     // actions 
+    actions: {
+        addTodo(item) {
+            this.todoList.push({ item, id: this.id++, completed: false })
+        },
+        deleteTodo(itemId) {
+            this.todoList = this.todoList.filter((object) => {
+                return object.id !== itemId
+            })
+        }
+    }
 })
