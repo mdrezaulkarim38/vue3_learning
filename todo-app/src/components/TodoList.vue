1bcc8f669294cf8d99157f18c3ecc4e141646df6
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-15 21:06:28 +0600

    delete is working

--- todo-app/src/components/TodoList.vue
+++ todo-app/src/components/TodoList.vue
@@ -3,7 +3,7 @@ import { useTodoListStore } from '@/stores/todoList'
 import { storeToRefs } from 'pinia'
 const store = useTodoListStore()
 const { todoList } = storeToRefs(store)
-const { toggleCompleted } = store
+const { toggleCompleted,deleteTodo } = store
 </script>
 
 <template>
@@ -11,11 +11,27 @@ const { toggleCompleted } = store
         <div class="content">
             <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
             <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+            <span @click="deleteTodo(todo.id)" class="x">&#10060;</span>
         </div>
     </div>
 </template>
 
 <style scoped>
+span {
+    margin: 0 10px;
+    cursor: pointer;
+}
+.item {
+    display: flex;
+    justify-content: center;
+}
+.content {
+    display: flex;
+    font-size: 1.5em;
+    justify-content: space-between;
+    width: 80vw;
+    padding: 5px;
+}
 .completed {
   text-decoration: line-through;
 }
